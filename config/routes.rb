b769a79da6161b92eb264afd9bad94ddeab0c8c6
xxKeefer <xxkeefer.code@gmail.com>
2020-06-29 15:20:41 +1000

    implemented devise gem and set up user db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/', to: 'welcome#index', as: 'root'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20200629051945_add_bnet_to_user.rb
@@ -0,0 +1,5 @@
+class AddBnetToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :bnet, :string
+  end
+end
